9e46531959c162aad664417bf69b5be929a7a2b7
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-02-12 11:11:27 +0100

    Add cors to app.js

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const housesRoutes = require("./routes/housesRoutes");
 const pageNotFound = require('./middlewares/pageNotFound');
 const errorsHandler = require('./middlewares/errorsHandler');
+const cors = require('cors');
 
 // Creazione app express
 const app = express();
 const port = process.env.SERVER_PORT;
+
+// Cors
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
 
 // Middleware per rendere la cartella publica accessibile
 app.use(express.static('public'));
